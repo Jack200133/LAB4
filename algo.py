2615b0a26758a1f209acb82c9d45c1c0473d0cfa
Juan Angel Carrera <angelitocarrera@gmail.com>
2023-02-18 22:15:29 -0600

    Creacion de Matriz y plot
    Co-authored-by: Aristondo01 <Aristondo01@users.noreply.github.com>
    Co-authored-by: Daniel14gonc <Daniel14gonc@users.noreply.github.com>

--- algo.py
+++ algo.py
@@ -20,64 +20,88 @@ x_train, x_test, y_train, y_test = train_test_split(living,
                                                     test_size=0.2,
                                                     random_state=1234)
 ##-------------------------------------------
+def create_matrices(polynomial_degree):
+    temp_x =[]
+    temp_w =[]
+    temp_y = []
+    x = np.array([])
+    w = np.array([])
+    y = np.array([])
+    for i in range(0, polynomial_degree + 1):
+        temp_x.append(x_train**i)
+        temp_w.append([random.randint(1, 10)])
+    
+    for element in y_train:
+        temp_y.append([element])
 
+    return np.array(temp_x), np.array(temp_w), np.array(temp_y)
 ### Task 1.2
-x = np.array([x_train**0,
-              x_train,
-              x_train**2,
-              x_train**3,
-              x_train**4])       
+
+polynomial_degree = 3
+
+x, w, Y = create_matrices(polynomial_degree)  
 
 X = x.transpose()
 
-w = np.array([[random.randint(1,10)], 
-                [random.randint(1,10)],
-                [random.randint(1,10)],
-                [random.randint(1,10)]])
-
 ##------------Fin Task 1.2------------
 
-#aa=np.array([1,2],[2,1])
-#bb=np.array([3,3])
-
-print(X[0] * w)
-
-# Task 1.3
-'''
-Y_predicted = (w_1 * X + w_0)
-
-iterations = 1000
-last_error = 1000
+iterator = 5000
 i = 0
+error = 0
+# L = 0.00000000000001
+L="0.0"
 
-error = 1
+L += (3**(polynomial_degree + 2))*"0"+"1"
+L = float(L)
 
-L = 0.0000001
-while (i < iterations and error > 0.005):
-    Y_predicted = w_1 * X + w_0
+# Valor para polinomio 2
+# L = 0.000000000000001
+
+# Valor para polinomio 3
+# L = 0.0000000000000000000001
+
+
+n = len(x_train)
+
+
+while i < iterator:
     
-    Dm = (-2.0/n)*sum(X*(Y - Y_predicted))
-    Dc = (-2.0/n)*sum(Y - Y_predicted)
+    # t = w.transpose() @ x
     
-    suma = 0
-    for i in range(len(Y)):
-        suma += (Y[i] - Y_predicted[i]) ** 2
-    error = (1.0/n) * suma
+    error = (1.0/n) * ((Y - X @ w).transpose() @ (Y - X @ w))
 
-    w_0 = w_0 - L * Dc
-    w_1 = w_1 - L * Dm
+    w = w - L * ((-2/n) * (x @ (Y - X @ w)))
+    #w = w - (L/n) * (X.T @ (X @ w - t))
 
-    if last_error > error:
-        last_error = error
 
-    i+=1
+    i += 1
 
-Y_pred = w_1 * X + w_0
+print("Error : ",error[0][0])
 
-print("M = ",w_1)
-print("C = ",w_0)
+for i in w:
+    print(i)
 
-plt.scatter(X, Y)
-plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red')
+# x -> x_train
+print(w)
+print(len(X))
+
+
+y_pred = (X @ w).transpose()
+print(y_pred[0])
+
+
+mymodel = np.poly1d(np.polyfit(x_train, y_pred[0], polynomial_degree))
+
+myline = np.linspace(1, 14000)
+
+sortedx = np.sort(x_train)
+sortedy = np.sort(y_pred[0])
+
+plt.scatter(x_train, y_train)
+plt.plot(myline, mymodel(myline))
 plt.show()
-'''
+"""
+plt.plot(sortedx, sortedy, color = 'green')
+plt.scatter(x_train, y_train) 
+# plt.plot([min(x_train), max(x_train)], [min(ylist), max(ylist)], color='red')  # regression line
+plt.show()"""
